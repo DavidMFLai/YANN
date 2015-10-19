34fbc4a7d02a0ed9bdca26fb523b13c160c9857f
David M.F. Lai <davidmflai.git@outlook.com>
2015-10-19 23:21:41 +0800

    Test case for ANN

--- CPPANN/CPPANN.cpp
+++ CPPANN/CPPANN.cpp
@@ -16,115 +16,44 @@
 using namespace std;
 using namespace CPPANN;
 
-TEST(Basics, DISABLED_temp)
+TEST(Basics, mattmazur)
 {
-	ANN<double> ann;
-	ann.add_layer(5); //5 neurons
-	ann.add_weights({
-		{ 1.11, 1.12, 1.13, 1.14 }, 
-		{ 1.21, 1.22, 1.23, 1.24 },
-		{ 1.31, 1.32, 1.33, 1.34 },
-		{ 1.41, 1.42, 1.43, 1.44 },
-		{ 1.51, 1.52, 1.53, 1.54 }
-	});
-	ann.add_layer(4); //4 neurons
-	ann.add_weights({
-		{ 2.11, 2.12, 2.13 },
-		{ 2.21, 2.22, 2.23 },
-		{ 2.31, 2.32, 2.33 },
-		{ 2.41, 2.42, 2.43 },
-	});
-	ann.add_layer(3); //3 neurons
-	ann.add_weights({
-		{ 3.11, 3.12 },
-		{ 3.21, 3.22 },
-		{ 3.31, 3.32 },
-	});
-	ann.add_layer(2); //2 neurons
-	 
-	for (int i = 0; i < 1; i++) {
-		ann.forward_propagate({ 0.11, 0.12, 0.13, 0.14, 0.15 });
-		ann.back_propagate(Matrix<double>{ {1., 0.} });
-	}
-}
+	//See http://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
 
-TEST(Basics, DISABLED_XORWithGoodInitials)
-{
-	ANN<double> ann;
-	ann.add_layer(2); //2 neurons
-	ann.add_weights({
-		{ 10., 10. },
-		{ 10., 10. },
-	});
-
-	ann.add_layer(2); //2 neurons
-	ann.add_bias({ -5., -15. });
-
-	ann.add_weights({
-		{ 10. }, //weights from the 0th neuron of the present layer
-		{ -20. },
-	});
-
-	ann.add_layer(1); //1 neuron
-	ann.add_bias({ -5. });
-
-	std::vector<double> tmp1, tmp2, tmp3, tmp4;
-	for (int i = 0; i < 10000; i++) {
-		tmp1 = ann.forward_propagate({ 0.99, 0.98 });
-		ann.back_propagate(Matrix<double>{ {0.} });
-		tmp2 = ann.forward_propagate({ 0.01, 0.01 });
-		ann.back_propagate(Matrix<double>{ {0.} });
-		tmp3 = ann.forward_propagate({ 0.01, 0.99 });
-		ann.back_propagate(Matrix<double>{ {1.} });
-		tmp4 = ann.forward_propagate({ 0.99, 0.01 });
-		ann.back_propagate(Matrix<double>{ {1.} });
-	}
-
-	tmp1 = ann.forward_propagate({ 0.99, 0.99 });
-	ann.back_propagate(Matrix<double>{ {0.} });
-
-	tmp2 = ann.forward_propagate({ 0.01, 0.99 });
-	ann.back_propagate(Matrix<double>{ {1.} });
-
-	tmp3 = ann.forward_propagate({ 0.99, 0.01 });
-	ann.back_propagate(Matrix<double>{ {1.} });
-
-	tmp4 = ann.forward_propagate({ 0.01, 0.01 });
-	ann.back_propagate(Matrix<double>{ {0.} });
-
-	auto &biases = ann.getBiases();
-	auto &weights = ann.getWeights();
-}
-
-
-TEST(Basics, DISABLED_mattmazur)
-{
+	//Setup ANN
 	ANN<double> ann;
 	ann.add_layer(2); //2 neurons
 	ann.add_weights({ 
 		{ 0.15, 0.25 }, 
 		{ 0.20, 0.30 },
 	});
-
 	ann.add_layer(2); //2 neurons
 	ann.add_bias({ 0.35, 0.35 });
-	
 	ann.add_weights({
 		{ 0.40, 0.50 }, //weights from the 0th neuron of the present layer
 		{ 0.45, 0.55 },
 	});
-
 	ann.add_layer(2); //1 neuron
 	ann.add_bias({ 0.60, 0.60 });
 
-	std::vector<double> tmp1;
-
-	tmp1 = ann.forward_propagate({ 0.05, 0.10 });
+	//Execute ANN
+	ann.forward_propagate({ 0.05, 0.10 });
 	ann.back_propagate(Matrix<double>{ { 0.01, 0.99} });
 
+	//Verify
+	double tolerence = 0.00000001;
+	auto &weights = ann.getWeights();
+	EXPECT_NEAR(0.14978072, weights[0](0, 0), tolerence);
+	EXPECT_NEAR(0.24975114, weights[0](0, 1), tolerence);
+	EXPECT_NEAR(0.19956143, weights[0](1, 0), tolerence);
+	EXPECT_NEAR(0.29950229, weights[0](1, 1), tolerence);
+	EXPECT_NEAR(0.35891648, weights[1](0, 0), tolerence);
+	EXPECT_NEAR(0.51130127, weights[1](0, 1), tolerence);
+	EXPECT_NEAR(0.40866619, weights[1](1, 0), tolerence);
+	EXPECT_NEAR(0.56137012, weights[1](1, 1), tolerence);
 }
 
-TEST(Basics, RANDXOR)
+TEST(Basics, DISABLED_RANDXOR)
 {
 	std::random_device rd;
 	std::mt19937 gen(rd());
