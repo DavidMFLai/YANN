1240275878b1c2ec7adc855d4a3a5eb0bf2ba9f7
David M.F. Lai <davidmflai.git@outlook.com>
2015-10-29 01:36:59 +0800

    Added a new test and its corresponding python check.

--- CPPANN/ANNTest.cpp
+++ CPPANN/ANNTest.cpp
@@ -18,7 +18,7 @@ using namespace CPPANN;
 
 TEST(Basics, mattmazur)
 {
-	//See http://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
+	//See http://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/. But the guy didnt update the biases
 
 	//Setup ANN
 	ANNBuilder<double> ann_builder;
@@ -52,6 +52,147 @@ TEST(Basics, mattmazur)
 	EXPECT_NEAR(0.51130127, weights[1](0, 1), tolerence);
 	EXPECT_NEAR(0.40866619, weights[1](1, 0), tolerence);
 	EXPECT_NEAR(0.56137012, weights[1](1, 1), tolerence);
+}
+
+TEST(Basics, CounterCheckInPython)
+{
+	//Setup ANN
+	ANNBuilder<double> ann_builder;
+	auto ann = ann_builder.set_layer(0, 5)
+		.set_layer(1, 4)
+		.set_layer(2, 3)
+		.set_layer(3, 2)
+		.set_weights(0, {
+			{ 0.01, 0.02, 0.03, 0.04 },
+			{ 0.05, 0.06, 0.07, 0.08 },
+			{ 0.09, 0.10, 0.11, 0.12 },
+			{ 0.13, 0.14, 0.15, 0.16 },
+			{ 0.17, 0.18, 0.19, 0.20 },
+		})
+		.set_weights(1, {
+			{ 0.25, 0.26, 0.27 },
+			{ 0.28, 0.29, 0.30 },
+			{ 0.31, 0.32, 0.33 },
+			{ 0.34, 0.35, 0.36 },
+		})
+		.set_weights(2, {
+			{ 0.40, 0.41 },
+			{ 0.42, 0.43 },
+			{ 0.44, 0.45 },
+		})
+		.set_bias(0, { 0.21, 0.22, 0.23, 0.24 })
+		.set_bias(1, { 0.37, 0.38, 0.39 })
+		.set_bias(2, { 0.46, 0.47 })
+		.build();
+
+	ann.forward_propagate({ 0.18, 0.29, 0.40, 0.51, 0.62 });
+	ann.back_propagate(Matrix<double>{ { 0.01, 0.99 }});
+
+	//Verify
+	double tolerence = 0.00000001;
+	auto &weights = ann.getWeights();
+	EXPECT_NEAR(0.00987499, weights[0](0, 0), tolerence);
+	EXPECT_NEAR(0.0198615, weights[0](0, 1), tolerence);
+	EXPECT_NEAR(0.02984825, weights[0](0, 2), tolerence);
+	EXPECT_NEAR(0.03983527, weights[0](0, 3), tolerence);
+	EXPECT_NEAR(0.0497986, weights[0](1, 0), tolerence);
+	EXPECT_NEAR(0.05977686, weights[0](1, 1), tolerence);
+	EXPECT_NEAR(0.06975552, weights[0](1, 2), tolerence);
+	EXPECT_NEAR(0.07973461, weights[0](1, 3), tolerence);
+	EXPECT_NEAR(0.0897222, weights[0](2, 0), tolerence);
+	EXPECT_NEAR(0.09969222, weights[0](2, 1), tolerence);
+	EXPECT_NEAR(0.10966279, weights[0](2, 2), tolerence);
+	EXPECT_NEAR(0.11963394, weights[0](2, 3), tolerence);
+	EXPECT_NEAR(0.12964581, weights[0](3, 0), tolerence);
+	EXPECT_NEAR(0.13960758, weights[0](3, 1), tolerence);
+	EXPECT_NEAR(0.14957005, weights[0](3, 2), tolerence);
+	EXPECT_NEAR(0.15953327, weights[0](3, 3), tolerence);
+	EXPECT_NEAR(0.16956941, weights[0](4, 0), tolerence);
+	EXPECT_NEAR(0.17952294, weights[0](4, 1), tolerence);
+	EXPECT_NEAR(0.18947732, weights[0](4, 2), tolerence);
+	EXPECT_NEAR(0.1994326, weights[0](4, 3), tolerence);
+	EXPECT_NEAR(0.24780606, weights[1](0, 0), tolerence);
+	EXPECT_NEAR(0.25773575, weights[1](0, 1), tolerence);
+	EXPECT_NEAR(0.26766959, weights[1](0, 2), tolerence);
+	EXPECT_NEAR(0.27778027, weights[1](1, 0), tolerence);
+	EXPECT_NEAR(0.28770913, weights[1](1, 1), tolerence);
+	EXPECT_NEAR(0.29764219, weights[1](1, 2), tolerence);
+	EXPECT_NEAR(0.30775465, weights[1](2, 0), tolerence);
+	EXPECT_NEAR(0.31768269, weights[1](2, 1), tolerence);
+	EXPECT_NEAR(0.32761498, weights[1](2, 2), tolerence);
+	EXPECT_NEAR(0.33772922, weights[1](3, 0), tolerence);
+	EXPECT_NEAR(0.34765645, weights[1](3, 1), tolerence);
+	EXPECT_NEAR(0.35758797, weights[1](3, 2), tolerence);
+	EXPECT_NEAR(0.35310712, weights[2](0, 0), tolerence);
+	EXPECT_NEAR(0.4204483, weights[2](0, 1), tolerence);
+	EXPECT_NEAR(0.3727042, weights[2](1, 0), tolerence);
+	EXPECT_NEAR(0.44053808, weights[2](1, 1), tolerence);
+	EXPECT_NEAR(0.39230839, weights[2](2, 0), tolerence);
+	EXPECT_NEAR(0.46062627, weights[2](2, 1), tolerence);
+
+	auto &biases = ann.getBiases();
+	EXPECT_NEAR(0.2093055, biases[0](0, 0), tolerence);
+	EXPECT_NEAR(0.21923054, biases[0](0, 1), tolerence);
+	EXPECT_NEAR(0.22915696, biases[0](0, 2), tolerence);
+	EXPECT_NEAR(0.23908484, biases[0](0, 3), tolerence);
+	EXPECT_NEAR(0.36638459, biases[1](0, 0), tolerence);
+	EXPECT_NEAR(0.37626872, biases[1](0, 1), tolerence);
+	EXPECT_NEAR(0.38615969, biases[1](0, 2), tolerence);
+	EXPECT_NEAR(0.39749299, biases[2](0, 0), tolerence);
+	EXPECT_NEAR(0.48392732, biases[2](0, 1), tolerence);
+}
+
+TEST(Basics, Accending_and_decending)
+{
+	//Setup ANN
+	ANNBuilder<double> ann_builder;
+	auto ann = ann_builder.set_layer(0, 5)
+		.set_layer(1, 4)
+		.set_layer(2, 3)
+		.set_layer(3, 2)
+		.set_weights(0, {
+			{ 0.01, 0.02, 0.03, 0.04 },
+			{ 0.05, 0.06, 0.07, 0.08 },
+			{ 0.09, 0.10, 0.11, 0.12 },
+			{ 0.13, 0.14, 0.15, 0.16 },
+			{ 0.17, 0.18, 0.19, 0.20 },
+		})
+		.set_weights(1, {
+			{ 0.25, 0.26, 0.27 },
+			{ 0.28, 0.29, 0.30 },
+			{ 0.31, 0.32, 0.33 },
+			{ 0.34, 0.35, 0.36 },
+		})
+		.set_weights(2, {
+			{ 0.40, 0.41 },
+			{ 0.42, 0.43 },
+			{ 0.44, 0.45 },
+		})
+		.set_bias(0, { 0.21, 0.22, 0.23, 0.24 })
+		.set_bias(1, { 0.37, 0.38, 0.39 })
+		.set_bias(2, { 0.46, 0.47 })
+		.build();
+
+	std::vector<double> low_to_high_input = { 0.18, 0.29, 0.40, 0.51, 0.62 };
+	std::vector<double> low_to_high_expected_result = { 0.01, 0.99 };
+	std::vector<double> high_to_low_input = { 0.62, 0.51, 0.40, 0.29, 0.18 };
+	std::vector<double> high_to_low_expected_result = { 0.99, 0.01 };
+
+	for (int i = 0; i < 1000000; i++) {
+		ann.forward_propagate(low_to_high_input);
+		ann.back_propagate(low_to_high_expected_result);
+		ann.forward_propagate(high_to_low_input);
+		ann.back_propagate(high_to_low_expected_result);
+	}
+
+	std::vector<double> low_to_high_output = ann.forward_propagate(low_to_high_input);
+	std::vector<double> high_to_low_output = ann.forward_propagate(high_to_low_input);
+
+	double tolerence = 0.05;
+	EXPECT_NEAR(0.01, low_to_high_output[0], tolerence);
+	EXPECT_NEAR(0.99, low_to_high_output[1], tolerence);
+	EXPECT_NEAR(0.99, high_to_low_output[0], tolerence);
+	EXPECT_NEAR(0.01, high_to_low_output[1], tolerence);
 }
 
 TEST(Basics, XOR_SIGMOID)
---
+++ CrossChecks/CounterCheckInPython.py
@@ -0,0 +1,152 @@
+#Mirrors "TEST(Basics, CounterCheckInPython)" in ANNTest.cpp
+import math 
+import numpy as np
+ 
+def sigmoid(x):
+    return 1/(1+math.exp(-x))
+    
+def sigmoid_prime(sigmoid_value):
+    return sigmoid_value*(1-sigmoid_value)  
+    
+sigmoid_matrix = np.frompyfunc(sigmoid, 1, 1)    
+sigmoid_prime_matrix = np.frompyfunc(sigmoid_prime, 1, 1)
+    
+s0 = np.matrix([[0.18, 0.29, 0.40, 0.51, 0.62]])
+w0 = np.matrix([[0.01, 0.02, 0.03, 0.04],
+                [0.05, 0.06, 0.07, 0.08],
+                [0.09, 0.10, 0.11, 0.12],
+                [0.13, 0.14, 0.15, 0.16],
+                [0.17, 0.18, 0.19, 0.20]])
+b0 = np.matrix([ 0.21, 0.22, 0.23, 0.24 ])
+w1 = np.matrix([[ 0.25, 0.26, 0.27 ],
+			[ 0.28, 0.29, 0.30 ],
+			[ 0.31, 0.32, 0.33 ],
+			[ 0.34, 0.35, 0.36 ]])
+b1 = np.matrix([0.37, 0.38, 0.39])
+w2 = np.matrix([[ 0.40, 0.41 ],
+			[ 0.42, 0.43 ],
+			[ 0.44, 0.45 ]])
+b2 = np.matrix([ 0.46, 0.47])
+
+#forward
+s1 = sigmoid_matrix(s0*w0+b0)
+s2 = sigmoid_matrix(s1*w1+b1)
+s3 = sigmoid_matrix(s2*w2+b2)
+
+print 'forward output =', s3
+
+#backward
+speed = 0.5
+expected = np.matrix('0.01 0.99')
+
+#Find b2_update
+dETotal_dS3 = s3 - expected
+dS3_dN2 = sigmoid_prime_matrix(s3)
+dN2_dB2 = np.ones((1, 2))
+dETotal_dB2 = np.multiply(dETotal_dS3, dS3_dN2, dN2_dB2)
+b2_update = dETotal_dB2 * speed
+print 'b2_update =', b2_update
+
+#Find w2_update
+dETotal_dS3 = s3 - expected
+dS3_dN2 = sigmoid_prime_matrix(s3)
+dETotal_dN2 = np.multiply(dETotal_dS3, dS3_dN2)
+dN2_dW2 = np.concatenate((s2, s2), 0).transpose()
+dETotal_dW2 = np.matrix(np.zeros(dN2_dW2.shape))
+dETotal_dW2[:,0] = np.multiply(dN2_dW2[:,0], dETotal_dN2[:,0])
+dETotal_dW2[:,1] = np.multiply(dN2_dW2[:,1], dETotal_dN2[:,1])
+w2_update = dETotal_dW2 * speed
+print 'w2_update =', w2_update
+
+#Find b1_update
+dETotal_dS3 = s3 - expected
+dS3_dN2 = sigmoid_prime_matrix(s3)
+dETotal_dN2 = np.multiply(dETotal_dS3, dS3_dN2)
+dN2_dS2 = w2.transpose()
+dETotal_dS2 = np.matrix(np.zeros(s2.shape))
+dETotal_dS2 = dETotal_dS2 + np.multiply(dN2_dS2[0, :], dETotal_dN2[:, 0])
+dETotal_dS2 = dETotal_dS2 + np.multiply(dN2_dS2[1, :], dETotal_dN2[:, 1])
+dS2_dN1 = sigmoid_prime_matrix(s2)
+dN1_dB1 = np.ones(b1.shape)
+dETotal_dB1 = np.multiply(dETotal_dS2, dS2_dN1, dN1_dB1)
+b1_update = dETotal_dB1 * speed
+print 'b1_update =', b1_update
+
+#Find w1_update
+dETotal_dS3 = s3 - expected
+dS3_dN2 = sigmoid_prime_matrix(s3)
+dETotal_dN2 = np.multiply(dETotal_dS3, dS3_dN2)
+dN2_dS2 = w2.transpose()
+dETotal_dS2 = np.matrix(np.zeros(s2.shape))
+dETotal_dS2 = dETotal_dS2 + np.multiply(dN2_dS2[0, :], dETotal_dN2[:, 0])
+dETotal_dS2 = dETotal_dS2 + np.multiply(dN2_dS2[1, :], dETotal_dN2[:, 1])
+dS2_dN1 = sigmoid_prime_matrix(s2)
+dETotal_dN1 = np.multiply(dETotal_dS2, dS2_dN1)
+dN1_dW1 = np.concatenate((s1, s1, s1), 0).transpose()
+dETotal_dW1 = np.matrix(np.zeros(dN1_dW1.shape))
+dETotal_dW1[:,0] = np.multiply(dN1_dW1[:,0], dETotal_dN1[:,0])
+dETotal_dW1[:,1] = np.multiply(dN1_dW1[:,1], dETotal_dN1[:,1])
+dETotal_dW1[:,2] = np.multiply(dN1_dW1[:,2], dETotal_dN1[:,2])
+w1_update = dETotal_dW1 * speed
+print 'w1_update =', w1_update
+
+#find b0_update
+dETotal_dS3 = s3 - expected
+dS3_dN2 = sigmoid_prime_matrix(s3)
+dETotal_dN2 = np.multiply(dETotal_dS3, dS3_dN2)
+dN2_dS2 = w2.transpose()
+dETotal_dS2 = np.matrix(np.zeros(s2.shape))
+dETotal_dS2 = dETotal_dS2 + np.multiply(dN2_dS2[0, :], dETotal_dN2[:, 0])
+dETotal_dS2 = dETotal_dS2 + np.multiply(dN2_dS2[1, :], dETotal_dN2[:, 1])
+dS2_dN1 = sigmoid_prime_matrix(s2)
+dETotal_dN1 = np.multiply(dETotal_dS2, dS2_dN1)
+dN1_dS1 = w1.transpose()
+dETotal_dS1 = np.matrix(np.zeros(s1.shape))
+dETotal_dS1 = dETotal_dS1 + np.multiply(dN1_dS1[0, :], dETotal_dN1[:, 0])
+dETotal_dS1 = dETotal_dS1 + np.multiply(dN1_dS1[1, :], dETotal_dN1[:, 1])
+dETotal_dS1 = dETotal_dS1 + np.multiply(dN1_dS1[2, :], dETotal_dN1[:, 2])
+dS1_dN0 = sigmoid_prime_matrix(s1)
+dN0_dB0 = np.ones(b0.shape)
+dETotal_dB0 = np.multiply(dETotal_dS1, dS1_dN0, dN0_dB0)
+b0_update = dETotal_dB0 * speed
+print 'b0_update =', b0_update
+
+#find w0_update
+dETotal_dS3 = s3 - expected
+dS3_dN2 = sigmoid_prime_matrix(s3)
+dETotal_dN2 = np.multiply(dETotal_dS3, dS3_dN2)
+dN2_dS2 = w2.transpose()
+dETotal_dS2 = np.matrix(np.zeros(s2.shape))
+dETotal_dS2 = dETotal_dS2 + np.multiply(dN2_dS2[0, :], dETotal_dN2[:, 0])
+dETotal_dS2 = dETotal_dS2 + np.multiply(dN2_dS2[1, :], dETotal_dN2[:, 1])
+dS2_dN1 = sigmoid_prime_matrix(s2)
+dETotal_dN1 = np.multiply(dETotal_dS2, dS2_dN1)
+dN1_dS1 = w1.transpose()
+dETotal_dS1 = np.matrix(np.zeros(s1.shape))
+dETotal_dS1 = dETotal_dS1 + np.multiply(dN1_dS1[0, :], dETotal_dN1[:, 0])
+dETotal_dS1 = dETotal_dS1 + np.multiply(dN1_dS1[1, :], dETotal_dN1[:, 1])
+dETotal_dS1 = dETotal_dS1 + np.multiply(dN1_dS1[2, :], dETotal_dN1[:, 2])
+dS1_dN0 = sigmoid_prime_matrix(s1)
+dETotal_dN0 = np.multiply(dETotal_dS1, dS1_dN0)
+dN0_dW0 = np.concatenate((s0, s0, s0, s0), 0).transpose()
+dETotal_dW0 = np.matrix(np.zeros(dN0_dW0.shape))
+dETotal_dW0[:,0] = np.multiply(dN0_dW0[:,0], dETotal_dN0[:,0])
+dETotal_dW0[:,1] = np.multiply(dN0_dW0[:,1], dETotal_dN0[:,1])
+dETotal_dW0[:,2] = np.multiply(dN0_dW0[:,2], dETotal_dN0[:,2])
+dETotal_dW0[:,3] = np.multiply(dN0_dW0[:,3], dETotal_dN0[:,3])
+w0_update = dETotal_dW0 * speed
+print 'w0_update =', w0_update
+
+#Actual updates
+b2 = b2 - b2_update
+print 'b2 =', b2
+w2 = w2 - w2_update
+print 'w2 =', w2
+b1 = b1 - b1_update
+print 'b1 =', b1
+w1 = w1 - w1_update
+print 'w1 =', w1
+b0 = b0 - b0_update
+print 'b0 =', b0
+w0 = w0 - w0_update
+print 'w0 =', w0
